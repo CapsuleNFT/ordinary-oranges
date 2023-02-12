aae8f42776e5168f36bb5c18a3ec227fa50f5143
curveballdaniel <curveballdaniel@gmail.com>
2023-02-12 01:45:41 -0500

    Update deploy script

--- deploy/OrdinaryOranges.ts
+++ deploy/OrdinaryOranges.ts
@@ -1,24 +1,36 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { ethers } from 'hardhat'
+import { LedgerSigner } from '@anders-t/ethers-ledger'
+import ICapsule from '../artifacts/contracts/interfaces/ICapsule.sol/ICapsule.json'
 
 const OrdinaryOranges = 'OrdinaryOranges'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
-  const { deploy } = deployments
-
+  const { execute, deploy } = deployments
+  
   const { deployer } = await getNamedAccounts()
 
-  await deploy(OrdinaryOranges, {
+  const capsuleCollectionAddress = "0x1b41F57D52FE6dB3a63bceB4E8845c0F9F31f859"
+
+  const OOContract = await deploy(OrdinaryOranges, {
     from: deployer,
     log: true,
-    args: [],
-    value: ethers.utils.parseEther('0.025'),
+    args: [capsuleCollectionAddress]
   })
 
-  // deploy
-  // force burn through contract
+  console.log("Deployment done!")
+  
+  const provider = new ethers.providers.JsonRpcProvider(process.env.NODE_URL)
+  const signer = new LedgerSigner(provider)
+  const capsuleCollectionContract = new ethers.Contract(capsuleCollectionAddress, ICapsule.abi, provider)
+
+  //await capsuleCollectionContract.connect(signer).transferOwnership(OOContract.address)
+
+  //await capsuleCollectionContract.connect(signer).updateTokenURIOwner(OOContract.address)
+
+  await execute(OrdinaryOranges, { from: deployer, log: true }, "claimCollectionBurnerRole");
 }
 export default func
 func.tags = [OrdinaryOranges]
